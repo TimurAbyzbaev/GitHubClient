a60c8e2bc9e51c455e7fad2379317dd3810da5e6
TimurHomePC <timstudent15@gmail.com>
2023-03-09 23:04:56 +0500

    Добавил базовый layout

--- app/src/main/java/com/example/githubclient/MainActivity.kt
+++ app/src/main/java/com/example/githubclient/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.githubclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.githubclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.refreshButton.setOnClickListener{
+            Toast.makeText(this, "Hello!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
