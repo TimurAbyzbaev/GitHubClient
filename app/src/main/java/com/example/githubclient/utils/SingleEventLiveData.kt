f90d0461d0e527da54e0a990d86d8648c213479a
Timur <timur@vlaga.pro>
2023-03-16 16:42:03 +0500

    SingleEvent

--- app/src/main/java/com/example/githubclient/utils/SingleEventLiveData.kt
+++ app/src/main/java/com/example/githubclient/utils/SingleEventLiveData.kt
@@ -1,10 +1,31 @@
 package com.example.githubclient.utils
 
+import android.util.Log
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
 
 class SingleEventLiveData<T>: MutableLiveData<T>() {
+    private val pending = AtomicBoolean(false)
 
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        if(hasObservers()){
+            Log.w(TAG, "Multiple observers registered but only one will be notified")
+        }
+        super.observe(owner, Observer { t ->
+            if(pending.compareAndSet(true, false)){
+                observer.onChanged(t)
+            }
+        })
+    }
 
+    override fun setValue(value: T) {
+        pending.set(true)
+        super.setValue(value)
+    }
+
+    companion object{
+        private val TAG = "SingleLiveEvent"
+    }
 }
