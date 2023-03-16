3dd20ec7751d2e667e60f6686fe4775dd75814c3
Timur <timur@vlaga.pro>
2023-03-16 17:18:09 +0500

    Retrofit

--- app/src/main/java/com/example/githubclient/domain/entities/UserEntity.kt
+++ app/src/main/java/com/example/githubclient/domain/entities/UserEntity.kt
@@ -1,7 +1,10 @@
 package com.example.githubclient.domain.entities
 
+import com.google.gson.annotations.SerializedName
+
 data class UserEntity(
     val login: String,
     val id: Long,
+    @SerializedName("avatar_url")
     val avatarUrl: String
 )
